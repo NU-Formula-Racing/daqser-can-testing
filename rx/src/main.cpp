486193a010dc1bf85c83a0ebd004c3be21359285
Shrimp-Chip <esample21@gmail.com>
2024-05-21 13:42:08 -0500

    Rx working with daqser lib

--- rx/src/main.cpp
+++ rx/src/main.cpp
@@ -5,7 +5,7 @@
 
 #define VERSION_ARGS(major, minor, patch) major, minor, patch
 
-#define SCHEMA_NAME "test_schema_v1.0.0.drive"
+#define SCHEMA_NAME "esp32dev_test"
 #define SCHEMA_VERSION VERSION_ARGS(1, 0, 0)
 
 VirtualTimerGroup timerGroup;
@@ -14,11 +14,19 @@ VirtualTimerGroup timerGroup;
 void recordData()
 {
   //  Prepare data from the CAN bus for serialization
-  // daqser::updateSignals();
+  daqser::updateSignals();
   // Serialize the data, and get the raw bytes
   std::vector<std::uint8_t> byteData = daqser::serializeFrame();
   // send the data over lora or something
   // send(byteData);
+  // print out the bytes
+  std::cout << "Data(length:" << byteData.size() << "): ";
+  for (std::uint8_t byte : byteData)
+  {
+    Serial.print(byte, HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
 
 void setup()
